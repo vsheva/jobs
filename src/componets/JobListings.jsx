84250dbff174ca55af64db672a9ce425923ea800
vsheva <valera.sheva@gmail.com>
2024-06-11 00:05:28 -0400

    set up proxy

--- src/componets/JobListings.jsx
+++ src/componets/JobListings.jsx
@@ -11,8 +11,9 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
       try {
-        const res = await fetch('http://localhost:5000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -24,11 +25,6 @@ const JobListings = ({ isHome = false }) => {
     fetchJobs();
   }, []);
 
-  let jobsi = jobs.map(job => {
-    console.log(job);
-    return <JobListing key={job.id} job={job} />;
-  });
-
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -39,7 +35,11 @@ const JobListings = ({ isHome = false }) => {
         {loading ? (
           <Spinner loading={loading} />
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">{jobsi}</div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map(job => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
         )}
       </div>
     </section>
