6c51fec6ab3e0a87fb68532f8f676695aef40475
vsheva <valera.sheva@gmail.com>
2024-06-11 22:43:44 -0400

    routing

--- src/componets/JobListing.jsx
+++ src/componets/JobListing.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const JobListing = job => {
+const JobListing = ({ job }) => {
   console.log(job);
   const [showDescription, setShowDescription] = useState(false);
 
-  let description = job.job.description;
+  let description = job.description;
   if (!showDescription) {
     description = description.substring(0, 150) + '...';
   }
@@ -15,8 +15,8 @@ const JobListing = job => {
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
         <div className="mb-6">
-          <div className="text-gray-600 my-2">{job.job.type}</div>
-          <h3 className="text-xl font-bold">{job.job.title}</h3>
+          <div className="text-gray-600 my-2">{job.type}</div>
+          <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
         <div className="mb-5">{description}</div>/
         <button
@@ -29,15 +29,15 @@ const JobListing = job => {
         >
           {showDescription ? 'less' : 'more'}
         </button>
-        <h3 className="text-indigo-500 mb-2">{job.job.salary} / Year</h3>
+        <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
         <div className="border border-gray-100 mb-5"></div>
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
             <FaMapMarker className="inline text-lg mb-1" />
-            {job.job.location}
+            {job.location}
           </div>
           <Link
-            to={`./jobs/${job.job.id}`} //в строке меняем JS
+            to={`/job/${job.id}`} //в строке меняем JS
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
